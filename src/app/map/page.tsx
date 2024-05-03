e65bffb80bb4729e43498f35e856b8649e0f5731
egorvts <egor.vetosh2310@gmail.com>
2024-05-03 22:41:53 +0500

    UI improvements

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,6 +1,13 @@
 import Map from "@/components/Map";
 
 const MapView = () => {
-  return <Map />;
+  return (
+    <>
+      <div className="absolute top-0 left-0 w-full h-16 bg-[#E5E3DF]/60 backdrop-blur-lg"></div>
+      <div>
+        <Map />
+      </div>
+    </>
+  );
 };
 export default MapView;
